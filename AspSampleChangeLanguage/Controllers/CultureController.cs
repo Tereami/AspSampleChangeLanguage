09bb653f05b69a2333a3939df09abbd557db6e54
Tereami <undress.my.future@gmail.com>
2025-12-28 22:43:37 +0300

    add culturecontroller

---
+++ AspSampleChangeLanguage/Controllers/CultureController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspSampleChangeLanguage.Controllers
+{
+    public class CultureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
