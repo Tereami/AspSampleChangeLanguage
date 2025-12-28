d1354a7ce76e86e17cbb639fd6402464bfae15d5
Tereami <undress.my.future@gmail.com>
2025-12-28 22:48:35 +0300

    add SetLanguage action

--- AspSampleChangeLanguage/Controllers/CultureController.cs
+++ AspSampleChangeLanguage/Controllers/CultureController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace AspSampleChangeLanguage.Controllers
 {
     public class CultureController : Controller
     {
-        public IActionResult Index()
+        public IActionResult SetLanguage(string culture, string returnUrl)
         {
-            return View();
+            string cookieName = CookieRequestCultureProvider.DefaultCookieName;
+            RequestCulture rc = new RequestCulture(culture);
+            string cookieValue = CookieRequestCultureProvider.MakeCookieValue(rc);
+            CookieOptions opt = new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) };
+
+            Response.Cookies.Append(cookieName, cookieValue, opt);
+
+            return LocalRedirect(returnUrl);
         }
     }
 }
