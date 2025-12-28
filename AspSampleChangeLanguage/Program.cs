2abb52ffd0fa473516740cf80296b61212e86acd
Tereami <undress.my.future@gmail.com>
2025-12-28 22:43:01 +0300

    add localization to program.cs

--- AspSampleChangeLanguage/Program.cs
+++ AspSampleChangeLanguage/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Globalization;
 
 namespace AspSampleChangeLanguage
 {
@@ -10,10 +12,37 @@ namespace AspSampleChangeLanguage
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddLocalization(o => o.ResourcesPath = "Resources");
+
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services
+                .AddControllersWithViews()
+                .AddViewLocalization()
+                .AddDataAnnotationsLocalization();
 
             var app = builder.Build();
+
+
+
+
+            var supportedCultures = new[]
+            {
+                new CultureInfo("ru"),
+                new CultureInfo("en")
+            };
+            var localizationOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("ru"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            };
+            localizationOptions.RequestCultureProviders.Insert(0,
+                new CookieRequestCultureProvider());
+            app.UseRequestLocalization(localizationOptions);
+
+
+
+
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
